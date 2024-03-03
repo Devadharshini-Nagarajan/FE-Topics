8dea4872dfdbf1a2395a5a810d2eaf5030527fd3
DevadharshiniN <nagarajan.de@northeastern.edu>
2024-03-03 17:56:52 -0500

    feat: tailwind added and initial css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home/Home";
+import MainLayout from "./components/MainLayout/MainLayout";
+import WebWorker from "./components/WebWorker/WebWorker";
 
 function App() {
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/web-worker",
+          element: <WebWorker />,
+        },
+      ],
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <RouterProvider router={routes} />
     </div>
   );
 }
